b72baa0bc85ca0b9ff9092a9f7282c8d38b9b0ff
Keks <keks@htmlacademy.ru>
2022-10-09 11:40:25 +0300

    :heavy_check_mark: Сборка #5

--- 5/js/data.js
+++ 5/js/data.js
@@ -1,37 +1,35 @@
-import { getRandomPositiveInteger } from './util.js';
-
-const COUNT_PHOTO = 25;
+import { getRandomArrayElement } from './util.js';
 
 const MESSAGES = [
-    'Всё отлично!',
-    'В целом всё неплохо. Но не всё.',
-    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
 const DESCRIPTIONS = [
-    'Осеннее настроение',
-    'Пишите, как проходит ваш день)',
-    'Мое лето',
-    'Лучший день в моей жизни!',
-    'За тобой хоть на край света',
-    'Прекрасный вечер',
-    'Точно особенный день'
+  'Осеннее настроение',
+  'Пишите, как проходит ваш день)',
+  'Мое лето',
+  'Лучший день в моей жизни!',
+  'За тобой хоть на край света',
+  'Прекрасный вечер',
+  'Точно особенный день'
 ];
 
 const NAMES = [
-    'Ольга',
-    'Кирилл',
-    'Андрей',
-    'Дарья',
-    'Мария',
-    'Максим',
-    'Евгений',
-    'Анастасия',
-    'Вера',
-    'Анна'
+  'Ольга',
+  'Кирилл',
+  'Андрей',
+  'Дарья',
+  'Мария',
+  'Максим',
+  'Евгений',
+  'Анастасия',
+  'Вера',
+  'Анна'
 ];
 
 let countLikes = {
@@ -39,30 +37,25 @@ let countLikes = {
     MAX: 200
 };
 
+MAX_PHOTOS = 25;
+MAX_COMMENTS = 6;
+
 //генерируем комментарии
-const createComment = (id) => ({
+const createComments = (id) => ({
     id,
     avatar: `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
     message: MESSAGES[getRandomPositiveInteger(0, MESSAGES.length - 1)],
     name: NAMES[getRandomPositiveInteger(0, NAMES.length - 1)]
 });
   
-//создаем данные фото
-const createPhotoData = (id) => ({
+const createPhotosData = (id) => ({
     id,
     url: `photos/${id}.jpg`,
-    description: DESCRIPTIONS[getRandomPositiveInteger(0, DESCRIPTIONS.length - 1)],
+    description: getRandomArrayElement(DESCRIPTIONS),
     likes: getRandomPositiveInteger(countLikes.MIN, countLikes.MAX),
-    comments: createComment(id)
-});
- 
-//добавляем данные к фотографиям  
-const generateData = () => {
-    const photos = [];
-    for(let i = 0; i < COUNT_PHOTO; i++) {
-        photos.push(createPhotoData(i + 1));
-    }
-    return photos;
-}
-
-export {generateData};
+    comments: Array.from({length: getRandomPositiveInteger(1, MAX_COMMENTS)}).map((element, index) => createComments(index + 1)),
+  });
+  
+const getPhotosData = () => Array.from({length: MAX_PHOTOS}).map((element, index) => createPhotosData(index + 1));
+  
+export {getPhotosData};
