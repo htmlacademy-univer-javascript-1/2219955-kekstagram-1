acce5fbb3b2af7b146ac14d497a2cdacc8001b60
Daria531 <daria.korelina@urfu.me>
2022-12-17 20:58:36 +0500

    module8-task1

---
+++ js/random-number.js
@@ -0,0 +1,9 @@
+//получение случайного числа
+const getRandomNumber = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+export{getRandomNumber};
